d897ca320b4aad9c23863214a86945c6319bde6f
jonjam1990@googlemail.com <jonjam1990@googlemail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-02-23 16:34:04 +0000

    - Application now using XML defined view - Data is scrollable in application.

--- code/src/com/googlecode/rich2012cafe/CaffeineFinder.java
+++ code/src/com/googlecode/rich2012cafe/CaffeineFinder.java
@@ -1,31 +1,28 @@
 package com.googlecode.rich2012cafe;
 
+import com.googlecode.rich2012cafe.controller.AppController;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.googlecode.rich2012cafe.controller.AppController;
-
 /**
  * N.B. In running and developing application need to increase Eclipse minimum/maximum memory requirements by altering
  * eclipse.ini file (Linux - located in /usr/lib/eclipse/) and change appropiate lines to this:
- * 		-Xms512m
- * 		-Xmx1024m
- * 
- * TODO WORK WITH VIEWS DEFINED IN XML FILE. 
+ *              -Xms512m
+ *              -Xmx1024m
  * 
  * @author Jonathan Harrison (jonjam1990@googlemail.com)
  */
 public class CaffeineFinder extends Activity {
-	
+
 	private AppController controller;
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        //TextView tv = (TextView) findViewById(R.id.textview); CAUSING NULLPOINTER
-        TextView tv = new TextView(this);
-        setContentView(tv);
+        setContentView(R.layout.main);
+        TextView tv = (TextView) findViewById(R.id.textview);
         
         controller = new AppController(tv);
         controller.runSPARQLQuery();

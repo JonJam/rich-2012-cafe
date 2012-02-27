67e8efa10cc2d33b08d74ac5aeeb611c705c5af9
mrman2289 <mrman2289@ef551103-5f9e-998d-499b-924edff78dac>
2012-02-27 15:51:34 +0000

    removed thee idea files from the repo as they aren't meant to be shared, my bad

---
+++ code/src/com/googlecode/rich2012cafe/GoogleMap.java
@@ -0,0 +1,28 @@
+package com.googlecode.rich2012cafe;
+
+import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+/**
+ * User: cs16g08
+ * Date: 27/02/2012
+ * Time: 15:30
+ */
+public class GoogleMap extends MapActivity {
+
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+}

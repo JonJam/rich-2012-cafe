240417d9f130ee1f746c306ea262d063e938cab1
mrman2289@gmail.com <mrman2289@gmail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-01 11:31:38 +0000

    Should now auto load map to Uni, listener for location changes is up, to fire stuff open terminal and telnet to the emulator (telnet localhost 5554 in my case) and call 'geo fix -1.39 50.938' for example

--- trunk/code/src/com/googlecode/rich2012cafe/view/MapViewInterface.java
+++ trunk/code/src/com/googlecode/rich2012cafe/view/MapViewInterface.java
@@ -1,10 +1,14 @@
 package com.googlecode.rich2012cafe.view;
 
+import com.google.android.maps.MapView;
+
 /**
  * User: cs16g08
  * Date: 27/02/2012
  * Time: 17:05
  */
 public interface MapViewInterface {
+
+    public MapView getMapView();
 
 }

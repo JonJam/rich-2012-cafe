0fe4a0a2171330766a771256bd9af6bfba17559f
mrman2289@gmail.com <mrman2289@gmail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-08 11:57:59 +0000

    quick fix to maps

--- code/src/com/googlecode/rich2012cafe/activities/GoogleMap.java
+++ code/src/com/googlecode/rich2012cafe/activities/GoogleMap.java
@@ -20,6 +20,8 @@ public class GoogleMap extends MapActivity implements MapViewInterface {
     private MapController mapController;
     private MapOverlays itemizedOverlays;
     private LocationManager locationManager;
+    
+    private Location currentBestLocation;
 
     @Override
     protected boolean isRouteDisplayed() {
@@ -36,18 +38,30 @@ public class GoogleMap extends MapActivity implements MapViewInterface {
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
 
-        getMapView().setBuiltInZoomControls(true);
-        
-//        String locationProvider = LocationManager.NETWORK_PROVIDER;
-        String locationProvider = LocationManager.GPS_PROVIDER; //TODO Craig: Change this back to network provider for real
 
-        boolean enabled = getLocationManager().isProviderEnabled(locationProvider);
+        getMapView().setBuiltInZoomControls(true);
+
+        String networkLocationProvider = LocationManager.NETWORK_PROVIDER;
+        String gpsLocationProvider = LocationManager.GPS_PROVIDER; //TODO Craig: Change this back to network provider for real
+
+        boolean enabled = getLocationManager().isProviderEnabled(gpsLocationProvider);
 
         // Check if enabled and if not send user to the GSP settings better solution would be to display a dialog and suggesting to go to the settings
+        //TODO Craig: Find if there is a better way to ask for GPS position
         if (!enabled) {
             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             startActivity(intent);
         }
+
+        Location lastGPSKnownLocation = locationManager.getLastKnownLocation(gpsLocationProvider);
+        Location lastNetworkKnownLocation = locationManager.getLastKnownLocation(networkLocationProvider);
+
+        currentBestLocation = handleQuickFixFromLastKnownLocation(lastGPSKnownLocation, lastNetworkKnownLocation);
+
+        if (currentBestLocation != null) {
+            updateMap(currentBestLocation);
+        }
+
 
 //        
        
@@ -73,15 +87,40 @@ public class GoogleMap extends MapActivity implements MapViewInterface {
         };
 
         //dummy default location
-        Location dummyLocation = new Location(locationProvider);
+        Location dummyLocation = new Location(gpsLocationProvider);
         dummyLocation.setLatitude(50.935396);
         dummyLocation.setLongitude(-1.396214);
         updateMap(dummyLocation);
 
 //        updateMap(getLocationManager().getLastKnownLocation(locationProvider));
 
-        getLocationManager().requestLocationUpdates(locationProvider, 0, 0, locationListener);
+        getLocationManager().requestLocationUpdates(gpsLocationProvider, 0, 0, locationListener);
 
+
+    }
+
+    private Location handleQuickFixFromLastKnownLocation(Location lastGPSKnownLocation, Location lastNetworkKnownLocation) {
+        Location quickFixLocation = null;
+
+        if (lastGPSKnownLocation != null && lastNetworkKnownLocation != null) {
+
+            if (lastGPSKnownLocation.hasAccuracy() && lastNetworkKnownLocation.hasAccuracy()) {
+                quickFixLocation = lastGPSKnownLocation.getAccuracy() <= lastNetworkKnownLocation.getAccuracy() ? lastGPSKnownLocation : lastNetworkKnownLocation;
+            }
+
+        } else if (lastGPSKnownLocation == null) {
+
+            if (lastNetworkKnownLocation != null) {
+                quickFixLocation = lastNetworkKnownLocation;
+            }
+
+        } else {
+
+            quickFixLocation = lastGPSKnownLocation;
+
+        }
+        
+        return quickFixLocation;
 
     }
 

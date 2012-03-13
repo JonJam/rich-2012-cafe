7c394c4a37e34ff77ef507ce40b66d074f304f26
p300ss <p300ss@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-13 23:10:18 +0000

    Reverting to PreferencesActivity for support on all versions, added ListPreferenceMultiSelect, so prefernce screen isnt one long list of checkboxes.

--- code/src/com/googlecode/rich2012cafe/activities/Preferences.java
+++ code/src/com/googlecode/rich2012cafe/activities/Preferences.java
@@ -1,57 +1,32 @@
 package com.googlecode.rich2012cafe.activities;
 
-import java.util.List;
-
 import com.googlecode.rich2012cafe.R;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.util.Log;
-/**
- * Resources used:
- * 	http://developer.android.com/reference/android/preference/PreferenceActivity.html
- * 
- * @author Pratik Patel (p300ss@gmail.com), Jonathan Harrison (jonjam1990@googlemail.com)
- */
-public class Preferences extends PreferenceActivity{
-	 
-	public static class UserSettingsFragment extends PreferenceFragment{
+
+public class Preferences extends PreferenceActivity implements OnSharedPreferenceChangeListener{
+
+        private ListPreference userTypePref;
         
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-           
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.user_settings);
-            
-        }
-    }
-	
-	public static class SourceSettingsFragment extends PreferenceFragment{
-        
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.source_settings);
-    	}
-    }
-	
-	public static class ProductSettingsFragment extends PreferenceFragment{
-        
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.product_settings);
-    	}
-    }
-	
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.addPreferencesFromResource(com.googlecode.rich2012cafe.R.xml.preferences);
+        // Get the custom preference
+        userTypePref = (ListPreference)this.getPreferenceScreen().findPreference("userPref");   
+        this.getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
     }
-    
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.preference_headers, target);
-    }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sp, String key) {
+                // TODO Auto-generated method stub
+                if(key.equals("userPref")){
+                        userTypePref.setSummary(sp.getString(key, "Student"));
+                }
+        }
 }

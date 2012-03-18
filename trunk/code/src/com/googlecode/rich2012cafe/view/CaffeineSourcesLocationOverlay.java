13c379cf5bb15cf72ad91410ff92f50d719042ee
mrman2289@gmail.com <mrman2289@gmail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-18 15:45:35 +0000

    Add the start of a details activity for when a map item is selected

--- trunk/code/src/com/googlecode/rich2012cafe/view/CaffeineSourcesLocationOverlay.java
+++ trunk/code/src/com/googlecode/rich2012cafe/view/CaffeineSourcesLocationOverlay.java
@@ -1,7 +1,10 @@
 package com.googlecode.rich2012cafe.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import com.google.android.maps.OverlayItem;
+import com.googlecode.rich2012cafe.activities.BuildingDetails;
 
 /**
  * User: CS
@@ -12,6 +15,20 @@ public class CaffeineSourcesLocationOverlay extends MapOverlays {
 
     public CaffeineSourcesLocationOverlay(Drawable defaultMarker, Context context) {
         super(defaultMarker, context);
+    }
+
+    protected boolean onTap(int index) {
+
+        OverlayItem item =  getOverlays().get(index);
+
+        Intent intent = new Intent(getContext(), BuildingDetails.class);
+
+//        Bundle b = new Bundle();
+//
+//        b.putInt();
+        getContext().startActivity(intent);
+
+        return true;
     }
 
 }

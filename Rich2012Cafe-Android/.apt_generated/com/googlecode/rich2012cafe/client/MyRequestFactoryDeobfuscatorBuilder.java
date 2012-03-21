3f243075d91712ccbd14b79ce9f49fbc70788427
jonjam1990@googlemail.com <jonjam1990@googlemail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-21 09:29:20 +0000

    Added notes about Shared folder issue and how to fix if have problems

--- Rich2012Cafe-Android/.apt_generated/com/googlecode/rich2012cafe/client/MyRequestFactoryDeobfuscatorBuilder.java
+++ Rich2012Cafe-Android/.apt_generated/com/googlecode/rich2012cafe/client/MyRequestFactoryDeobfuscatorBuilder.java
@@ -8,6 +8,9 @@ public final class MyRequestFactoryDeobfuscatorBuilder extends com.google.web.bi
 {
 withRawTypeToken("8KVVbwaaAtl6KgQNlOTsLCp9TIU=", "com.google.web.bindery.requestfactory.shared.ValueProxy");
 withRawTypeToken("FXHD5YU0TiUl3uBaepdkYaowx9k=", "com.google.web.bindery.requestfactory.shared.BaseProxy");
+withRawTypeToken("rRLPucjyjJL_oQUxiIKukrC0uCs=", "com.googlecode.rich2012cafe.shared.CaffeineProductProxy");
+withRawTypeToken("mMYTCj0a2RnNwV2w6eBaliBdCeM=", "com.googlecode.rich2012cafe.shared.CaffeineSourceProxy");
 withRawTypeToken("eU9vcJc_N6gwYWNveVVbH7uUEu4=", "com.googlecode.rich2012cafe.shared.MessageProxy");
+withRawTypeToken("_T$l7gTXjIiCfXJa8vNusku8yhU=", "com.googlecode.rich2012cafe.shared.OpeningTimeProxy");
 withRawTypeToken("lYcwXIk5vZ7v$26PwoIn4XMQNGs=", "com.googlecode.rich2012cafe.shared.RegistrationInfoProxy");
 }}

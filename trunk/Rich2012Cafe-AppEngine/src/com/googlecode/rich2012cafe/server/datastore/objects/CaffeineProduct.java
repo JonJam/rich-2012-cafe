6359f726e51ca80d1b898c75692902c5bea05842
jonjam1990@googlemail.com <jonjam1990@googlemail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-03-22 14:37:42 +0000

    Fixed issues from when refactored code to ensure registering of devices and accounts still works. Altered database structure so caffeine products are now split between CaffeineSourceProduct and CaffeineProduct objects for greater normal form and overcome problems of GQL not having SQL DISTINCT, GROUP BY clauses.

--- trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/datastore/objects/CaffeineProduct.java
+++ trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/datastore/objects/CaffeineProduct.java
@@ -5,162 +5,38 @@ import javax.persistence.Id;
 
 @Entity
 public class CaffeineProduct {
-	
-	//URI Identifier for a product at a caffeine source e.g. http://id.southampton.ac.uk/point-of-service/42-cafe#offering-Cappuchino-Staff
+
 	@Id
-	private String id;
-	//URI Identifier for a caffeine source e.g. http://id.southampton.ac.uk/vending-machine/71
-	private String caffeineSourceId;
 	private String name;
-	private double price;
-	private String currency;
 	private String productType;
-	//Determines whether Staff, Student or All price.
-	private String priceType;
+	private double caffeineContent;
 	
-	public CaffeineProduct(String id, String caffeineSourceId, String name, double price, String currency, String productType, String priceType){
-		this.id = id;
-		this.caffeineSourceId = caffeineSourceId;
+	public CaffeineProduct(String name, String productType, double caffeineContent){
 		this.name = name;
-		this.price = price;
-		this.currency = currency;
 		this.productType = productType;
-		this.priceType = priceType;
-	}
-
-	/**
-	 * Method to get id of product.
-	 * 
-	 * @return id (String object)
-	 */
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * Method to set id of product.
-	 * 
-	 * @param id (String object)
-	 */
-	public void setId(String id) {
-		this.id = id;
+		this.caffeineContent = caffeineContent;
 	}
 	
-	/**
-	 * Method to get id of caffeine source.
-	 * 
-	 * @return caffeineSourceId (String object)
-	 */
-	public String getCaffeineSourceId() {
-		return caffeineSourceId;
-	}
-
-	/**
-	 * Method to set id of caffeine source.
-	 * 
-	 * @param caffeineSourceId (String object)
-	 */
-	public void setCaffeineSourceId(String caffeineSourceId) {
-		this.caffeineSourceId = caffeineSourceId;
-	}
-
-	/**
-	 * Method to get name of product.
-	 * 
-	 * @return name (String object)
-	 */
 	public String getName() {
 		return name;
 	}
-
-	/**
-	 * Method to set name of product.
-	 * 
-	 * @param name (String object)
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	/**
-	 * Method to get price of product.
-	 * 
-	 * @return price (double object)
-	 */
-	public double getPrice() {
-		return price;
-	}
-
-	/**
-	 * Method to set price of product.
-	 * 
-	 * @param price (double object)
-	 */
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	/**
-	 * Method to get currency of product.
-	 * 
-	 * @return currency (String object)
-	 */
-	public String getCurrency() {
-		return currency;
-	}
-
-	/**
-	 * Method to set currency of product.
-	 * 
-	 * @param currency (String object)
-	 */
-	public void setCurrency(String currency) {
-		this.currency = currency;
-	}
-	
-	/**
-	 * Method to get type of product.
-	 * 
-	 * @return productType (String object)
-	 */
 	public String getProductType() {
 		return productType;
 	}
-
-	/**
-	 * Method to set type of product.
-	 * 
-	 * @param productType (String object)
-	 */
 	public void setProductType(String productType) {
 		this.productType = productType;
 	}
-	
-	/**
-	 * Method to get price type of product.
-	 * 
-	 * @return type (String object)
-	 */
-	public String getPriceType() {
-		return priceType;
+	public double getCaffeineContent() {
+		return caffeineContent;
+	}
+	public void setCaffeineContent(double caffeineContent) {
+		this.caffeineContent = caffeineContent;
 	}
 	
-	/**
-	 * Method to set price type of product.
-	 * 
-	 * @param priceType (String object)
-	 */
-	public void setPriceType(String priceType) {
-		this.priceType = priceType;
-	}
-	
-	/**
-	 * Method to get String representation of a CaffeineSource.
-	 * 
-	 * @return String value
-	 */
 	public String toString(){
-		return "id: " + id + " caffeineSourceId: " + caffeineSourceId + " price: " + price + " currency: " + currency
-				+ " productType: " + productType;
-	}
+		return "name: " + name + " productType: " + productType + " caffeineContent: " + caffeineContent;
+	}	
 }
--- trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/sparql/SPARQLQuerier.java
+++ trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/sparql/SPARQLQuerier.java
@@ -7,8 +7,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 import com.googlecode.rich2012cafe.server.datastore.objects.CaffeineProduct;
+import com.googlecode.rich2012cafe.server.datastore.objects.CaffeineSourceProduct;
 import com.googlecode.rich2012cafe.server.datastore.objects.CaffeineSource;
 import com.googlecode.rich2012cafe.server.datastore.objects.OpeningTime;
+import com.googlecode.rich2012cafe.server.utils.Rich2012CafeUtil;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
@@ -18,99 +20,16 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.Syntax;
 
 /**
- * Resources used:
- * 	- http://incubator.apache.org/jena/index.html
- * 
  * @author Jonathan Harrison (jonjam1990@googlemail.com), Samantha Kanza (samikanza@gmail.com)
  */
 public class SPARQLQuerier {
 	
-	//SPARQL Endpoint
-	private static final String DATA_SOUTHAMPTON_ENDPOINT = "http://sparql.data.southampton.ac.uk/";
+	private ArrayList<CaffeineProduct> products;
 	
-	//Fields
-	private static final String SOURCE_ID = "id";
-	private static final String SOURCE_NAME = "name";
-	private static final String SOURCE_BUILDING_ID = "buildingid";
-	private static final String SOURCE_BUILDING_NAME = "buildingname";
-	private static final String SOURCE_BUILDING_LAT = "buildinglat";
-	private static final String SOURCE_BUILDING_LONG = "buildinglong";
+	public SPARQLQuerier(){
+		products = new ArrayList<CaffeineProduct>();
+	}
 	
-	private static final String TIME_ID = "id";
-	private static final String TIME_OPENS = "opens";
-	private static final String TIME_CLOSES = "closes";
-	private static final String TIME_FROM = "from";
-	private static final String TIME_TO = "to";
-	private static final String TIME_DAY = "day";
-	
-	private static final String PRODUCT_ID = "id";
-	private static final String PRODUCT_NAME = "name";
-	private static final String PRODUCT_PRICE = "price";
-	
-	//Queries
-	private static final String CAFFEINE_SOURCES_QUERY = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
-			+ " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-			+ " PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>"
-			+ " PREFIX sr: <http://data.ordnancesurvey.co.uk/ontology/spatialrelations/>"
-			+ " PREFIX purl: <http://purl.org/goodrelations/v1#>"
-			+ " PREFIX rooms: <http://vocab.deri.ie/rooms#>"
-			+ " PREFIX pos: <http://id.southampton.ac.uk/generic-products-and-services/>"
-			+ " SELECT DISTINCT ?" + SOURCE_ID + " ?" + SOURCE_NAME + " ?" + SOURCE_BUILDING_ID + " ?" + SOURCE_BUILDING_NAME
-			+ " ?" + SOURCE_BUILDING_LAT + " ?" + SOURCE_BUILDING_LONG + " WHERE {"
-				+ " {"
-					+ " ?poscaffeine purl:includes pos:Caffeine ."
-					+ " ?poscaffeine purl:availableAtOrFrom ?" + SOURCE_ID + " ."
-					+ " ?" + SOURCE_ID + " rdfs:label ?" + SOURCE_NAME + " ."
-					+ " ?" + SOURCE_ID + " sr:within ?building ."
-					+ " ?building a rooms:Building ; rdfs:label ?" + SOURCE_BUILDING_NAME + " ."
-					+ " ?building skos:notation ?" + SOURCE_BUILDING_ID + " ."
-					+ " ?building geo:lat ?" + SOURCE_BUILDING_LAT + " ; geo:long ?" + SOURCE_BUILDING_LONG
-				+ " } UNION {"
-					+ " ?poscaffeine purl:includes pos:Caffeine ."
-					+ " ?poscaffeine purl:availableAtOrFrom ?" + SOURCE_ID + " ."
-					+ " ?" + SOURCE_ID + " rdfs:label ?" + SOURCE_NAME + " ."
-					+ " ?" + SOURCE_ID + " rdfs:label ?" + SOURCE_BUILDING_NAME + " ."
-					+ " ?" + SOURCE_ID + " geo:lat ?" + SOURCE_BUILDING_LAT + " ; geo:long ?" + SOURCE_BUILDING_LONG + " ."
-				+ " }"
-			+ " }";
-	
-	private static final String OPENING_TIMES_QUERY1 = "PREFIX gr: <http://purl.org/goodrelations/v1#>"
-			+ "PREFIX xs: <http://www.w3.org/2001/XMLSchema#>"
-			+ "SELECT DISTINCT * WHERE { <";
-	private static final String OPENING_TIMES_QUERY2 = "> gr:hasOpeningHoursSpecification ?" + TIME_ID + " ."
-				+ " ?" + TIME_ID + " gr:opens ?" + TIME_OPENS + " ; gr:closes ?" + TIME_CLOSES + " ; gr:validFrom ?" + TIME_FROM 
-				+ " ; gr:validThrough ?" + TIME_TO +" ; "
-				+ "gr:hasOpeningHoursDayOfWeek ?" + TIME_DAY + " . "
-				+ "FILTER('";
-	private static final String OPENING_TIMES_QUERY3 = "'^^xs:dateTime > ?" + TIME_FROM + " && '";
-	private static final String OPENING_TIMES_QUERY4 = "'^^xs:dateTime < ?" + TIME_TO + " )}";
-	
-	private static final String CAFFEINE_PRODUCTS_QUERY1 = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-				+ "PREFIX purl: <http://purl.org/goodrelations/v1#>"
-				+ "SELECT DISTINCT ?" + PRODUCT_ID + " ?" + PRODUCT_NAME + " ?" + PRODUCT_PRICE + " WHERE {"
-					+ " ?" + PRODUCT_ID + " purl:availableAtOrFrom <";
-	private static final String CAFFEINE_PRODUCTS_QUERY2 = "> ."
-				+ " ?" + PRODUCT_ID + " purl:includes ?product ."
-				+ " ?product rdfs:label ?" + PRODUCT_NAME + " ."
-				+ " ?" + PRODUCT_ID + " rdfs:label ?" + PRODUCT_PRICE + " ."
-				+ "FILTER (regex(?" + PRODUCT_NAME + " , '^coke | coke |^coffee | coffee |^tea | tea |^relentless | relentless "
-				+ "|^powerade | powerade |^lucozade | lucozade |^red bull | red bull |^frappe | frappe |^cappuchino "
-				+ "| cappuchino |^americano | americano |^latte | latte |^espresso | espresso |^iced teas | iced teas "
-				+ "|^speciality teas | speciality teas ', 'i')"
-				+ "&& regex(?" + PRODUCT_NAME + " , '^((?!decaf).)*$', 'i') && regex(?" + PRODUCT_NAME + " , '^((?!peppermint).)*$', 'i')"
-				+ "&& regex(?" + PRODUCT_NAME + " , '^((?!camomile).)*$', 'i'))}";
-	
-	//Types
-	private static final String POS_TYPE = "Point of Service";
-	private static final String VENDING_MACHINE_TYPE = "Vending Machine";
-	private static final String STAFF_TYPE = "Staff";
-	private static final String STUDENT_TYPE = "Student";
-	private static final String ALL_TYPE = "All";
-	private static final String SOFT_DRINK_TYPE = "Soft Drink";
-	private static final String COFFEE_TYPE = "Coffee";
-	private static final String TEA_TYPE = "Tea";
-	private static final String ENERGY_DRINK_TYPE = "Energy Drink";
-		
 	/**
 	 * Method to execute a SPARQL query and obtain sources of Caffeine as CaffeineSource objects.
 	 * 
@@ -121,8 +40,8 @@ public class SPARQLQuerier {
 		ArrayList<CaffeineSource> sources = new ArrayList<CaffeineSource>();
 		
 		//Execute query
-		Query query = QueryFactory.create(CAFFEINE_SOURCES_QUERY, Syntax.syntaxARQ);
-        QueryExecution qe = QueryExecutionFactory.sparqlService(DATA_SOUTHAMPTON_ENDPOINT, query);
+		Query query = QueryFactory.create(Rich2012CafeUtil.CAFFEINE_SOURCES_QUERY, Syntax.syntaxARQ);
+        QueryExecution qe = QueryExecutionFactory.sparqlService(Rich2012CafeUtil.DATA_SOUTHAMPTON_ENDPOINT, query);
         ResultSet caffeineSourcesResults = qe.execSelect();
 
 		//Iterate through results.
@@ -130,7 +49,7 @@ public class SPARQLQuerier {
         	
             QuerySolution solution = caffeineSourcesResults.next();
             
-            String id = solution.getResource(SOURCE_ID).getURI();
+            String id = solution.getResource(Rich2012CafeUtil.SOURCE_ID).getURI();
             String buildingNumber;
             int offCampus;
             String type;
@@ -138,14 +57,14 @@ public class SPARQLQuerier {
             if(id.contains("vending-machine")){
             	//Vending machine type
             	
-            	type = VENDING_MACHINE_TYPE;
+            	type = Rich2012CafeUtil.VENDING_MACHINE_TYPE;
             } else{
             	//Point of Service type
             	
-            	type = POS_TYPE;
+            	type = Rich2012CafeUtil.POS_TYPE;
             }
             
-            if(solution.get(SOURCE_BUILDING_ID) == null){
+            if(solution.get(Rich2012CafeUtil.SOURCE_BUILDING_ID) == null){
             	//No building number so set as off campus
             	
             	buildingNumber = "N/A";
@@ -153,16 +72,16 @@ public class SPARQLQuerier {
             } else{
             	//Building number so set as on campus
             	
-            	buildingNumber = solution.getLiteral(SOURCE_BUILDING_ID).getString();
+            	buildingNumber = solution.getLiteral(Rich2012CafeUtil.SOURCE_BUILDING_ID).getString();
             	offCampus = 0;
             }
             
             CaffeineSource source = new CaffeineSource(id, 
-            		solution.getLiteral(SOURCE_NAME).getString(), 
+            		solution.getLiteral(Rich2012CafeUtil.SOURCE_NAME).getString(), 
             		buildingNumber, 
-            		solution.getLiteral(SOURCE_BUILDING_NAME).getString(), 
-            		solution.getLiteral(SOURCE_BUILDING_LAT).getDouble(), 
-            		solution.getLiteral(SOURCE_BUILDING_LONG).getDouble(), type, offCampus);
+            		solution.getLiteral(Rich2012CafeUtil.SOURCE_BUILDING_NAME).getString(), 
+            		solution.getLiteral(Rich2012CafeUtil.SOURCE_BUILDING_LAT).getDouble(), 
+            		solution.getLiteral(Rich2012CafeUtil.SOURCE_BUILDING_LONG).getDouble(), type, offCampus);
             sources.add(source);
 
         }
@@ -183,12 +102,14 @@ public class SPARQLQuerier {
 	public ArrayList<OpeningTime> getCurrentOpeningTimes(String caffeineSourceId){
 
 		ArrayList<OpeningTime> openingTimes = new ArrayList<OpeningTime>();
-		String todayString = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Calendar.getInstance().getTime());
+		String todayString = new SimpleDateFormat(Rich2012CafeUtil.DATE_FORMAT).format(Calendar.getInstance().getTime());
 		
 		//Execute query
-		Query query = QueryFactory.create(OPENING_TIMES_QUERY1 + caffeineSourceId + OPENING_TIMES_QUERY2 +
-				todayString + OPENING_TIMES_QUERY3 + todayString + OPENING_TIMES_QUERY4, Syntax.syntaxARQ);
-        QueryExecution qe = QueryExecutionFactory.sparqlService(DATA_SOUTHAMPTON_ENDPOINT, query);
+		Query query = QueryFactory.create(Rich2012CafeUtil.OPENING_TIMES_QUERY1 + caffeineSourceId 
+				+ Rich2012CafeUtil.OPENING_TIMES_QUERY2 + todayString 
+				+ Rich2012CafeUtil.OPENING_TIMES_QUERY3 + todayString 
+				+ Rich2012CafeUtil.OPENING_TIMES_QUERY4, Syntax.syntaxARQ);
+        QueryExecution qe = QueryExecutionFactory.sparqlService(Rich2012CafeUtil.DATA_SOUTHAMPTON_ENDPOINT, query);
         ResultSet  openingTimesResults = qe.execSelect();
 
 		//Iterate through results
@@ -196,22 +117,24 @@ public class SPARQLQuerier {
         	
             QuerySolution solution = openingTimesResults.next();
             
-            String id = solution.getResource(TIME_ID).getURI();
-            String dayURI = solution.getResource(TIME_DAY).getURI();
+            String id = solution.getResource(Rich2012CafeUtil.TIME_ID).getURI();
+            String dayURI = solution.getResource(Rich2012CafeUtil.TIME_DAY).getURI();
             String day = dayURI.substring(dayURI.indexOf("#") + 1);
-            String openTime = solution.getLiteral(TIME_OPENS).getString();
-            String closeTime = solution.getLiteral(TIME_CLOSES).getString();
+            String openTime = solution.getLiteral(Rich2012CafeUtil.TIME_OPENS).getString();
+            String closeTime = solution.getLiteral(Rich2012CafeUtil.TIME_CLOSES).getString();
             Date validFrom = null;
             Date validTo = null;
             
     		try {
-    			validFrom = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(solution.getLiteral(TIME_FROM).getString());
+    			validFrom = new SimpleDateFormat(Rich2012CafeUtil.DATE_FORMAT).parse(
+    					solution.getLiteral(Rich2012CafeUtil.TIME_FROM).getString());
     		} catch (ParseException e) {
     			e.printStackTrace();
     		}
     		
     		try {
-    			validTo = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(solution.getLiteral(TIME_TO).getString());
+    			validTo = new SimpleDateFormat(Rich2012CafeUtil.DATE_FORMAT).parse(
+    					solution.getLiteral(Rich2012CafeUtil.TIME_TO).getString());
     		} catch (ParseException e) {
     			e.printStackTrace();
     		}
@@ -239,14 +162,14 @@ public class SPARQLQuerier {
 	 * @param caffeineSourceId (URI for caffeine source)
 	 * @return ArrayList of CaffeineProduct objects
 	 */
-	public ArrayList<CaffeineProduct> getCaffeineProducts(String caffeineSourceId){
+	public ArrayList<CaffeineSourceProduct> getCaffeineSourceProducts(String caffeineSourceId){
 		
-		ArrayList<CaffeineProduct> products = new ArrayList<CaffeineProduct>();
+		ArrayList<CaffeineSourceProduct> sourceProducts = new ArrayList<CaffeineSourceProduct>();
 		
 		//Execute query
-		Query query = QueryFactory.create(CAFFEINE_PRODUCTS_QUERY1 + caffeineSourceId + CAFFEINE_PRODUCTS_QUERY2, 
-				Syntax.syntaxARQ);
-        QueryExecution qe = QueryExecutionFactory.sparqlService(DATA_SOUTHAMPTON_ENDPOINT, query);
+		Query query = QueryFactory.create(Rich2012CafeUtil.CAFFEINE_PRODUCTS_QUERY1 + caffeineSourceId 
+				+ Rich2012CafeUtil.CAFFEINE_PRODUCTS_QUERY2, Syntax.syntaxARQ);
+        QueryExecution qe = QueryExecutionFactory.sparqlService(Rich2012CafeUtil.DATA_SOUTHAMPTON_ENDPOINT, query);
         ResultSet caffeineProductsResults = qe.execSelect();
 	
 		//Iterate through results
@@ -254,33 +177,31 @@ public class SPARQLQuerier {
   
         	QuerySolution solution = caffeineProductsResults.next();
         	
-        	String id = solution.getResource(PRODUCT_ID).getURI();
-     	   	String name = solution.getLiteral(PRODUCT_NAME).getString();
-     	   	
-            String priceString = solution.getLiteral(PRODUCT_PRICE).getString().replace(name, "");
-            
+        	//Creating CaffeineSourceProduct object.
+   			String name = solution.getLiteral(Rich2012CafeUtil.PRODUCT_NAME).getString();
+        	String id = solution.getResource(Rich2012CafeUtil.PRODUCT_ID).getURI();
+            String priceString = solution.getLiteral(Rich2012CafeUtil.PRODUCT_PRICE).getString().replace(name, "");
      	   	double price;
      	   	String currency;
-            String type;
-            String productType = "";
-
+            String priceType;
+            
             //Set correct type and obtain price information.
- 	   		if(id.endsWith(STAFF_TYPE)){
+ 	   		if(id.endsWith(Rich2012CafeUtil.STAFF_TYPE)){
  	   			//Staff Product
  	   			
- 	   			type = STAFF_TYPE;
+ 	   			priceType = Rich2012CafeUtil.STAFF_TYPE;
  	   			priceString = priceString.substring(priceString.indexOf("-") + 1, priceString.indexOf("(")).trim(); 
  	   			
- 	   		} else if(id.endsWith(STUDENT_TYPE)){
+ 	   		} else if(id.endsWith(Rich2012CafeUtil.STUDENT_TYPE)){
  	   			//Student Product
  	   			
- 	   			type = STUDENT_TYPE;
+ 	   			priceType = Rich2012CafeUtil.STUDENT_TYPE;
  	   			priceString= priceString.substring(priceString.indexOf("-") + 1, priceString.indexOf("(")).trim(); 
  	   			
  	   		} else{
  	   			//All Product.
  	   			
- 	   			type = ALL_TYPE;
+ 	   			priceType = Rich2012CafeUtil.ALL_TYPE;
  	   			priceString = priceString.substring(priceString.indexOf("-") + 1).trim();
  	   			
  	   		}
@@ -288,27 +209,73 @@ public class SPARQLQuerier {
    			currency = String.valueOf(priceString.toCharArray()[0]);
    			price = Double.parseDouble((priceString.substring(priceString.indexOf(currency) + 1).trim()));
  	   		
- 	   		String nameForComparison = name.toLowerCase();
- 	   		if(nameForComparison.contains("coke")){
- 	   			productType = SOFT_DRINK_TYPE;
- 	   		} else if(nameForComparison.contains("coffee")){
- 	   			productType = COFFEE_TYPE;
- 	   		} else if(nameForComparison.contains("tea")){
- 	   			productType = TEA_TYPE;
- 	   		} else if(nameForComparison.contains("relentless") || nameForComparison.contains("powerade") 
- 	   				|| nameForComparison.contains("lucozade") || nameForComparison.contains("red bull")){
- 	   			productType = ENERGY_DRINK_TYPE;
- 	   		} else{
- 	   			//All others such as frappe, cappuchino, americano, latte, esspresso
- 	   			productType = COFFEE_TYPE;
- 	   		}
- 	   		
-     	   	products.add(new CaffeineProduct(id, caffeineSourceId, name, price, currency, productType, type));
-     	   	
+   			if(!containsProduct(name)){
+   				//No existing CaffeineProduct so create one and add to products ArrayList
+   				
+   				products.add(createCaffeineProduct(name));
+   			}
+           
+   		 	sourceProducts.add(new CaffeineSourceProduct(id, caffeineSourceId, name, price, currency, priceType)); 
         }
         
         qe.close();
         
+		return sourceProducts;
+	}
+	
+	private boolean containsProduct(String name){
+		
+		for(CaffeineProduct p : products){
+			if(p.getName().equals(name)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private CaffeineProduct createCaffeineProduct(String name){
+		
+		String productType = "";
+		double caffeineContent;
+		String nameForComparison = name.toLowerCase();
+	
+   		if(nameForComparison.contains("coke")){
+   			//Soft Drink Product
+   			
+   			productType = Rich2012CafeUtil.SOFT_DRINK_TYPE;
+   			caffeineContent = Rich2012CafeUtil.SOFT_DRINK_CAFFEINE_CONTENT;
+   			
+   		} else if(nameForComparison.contains("coffee")){
+   			//Coffee Product
+   			
+   			productType = Rich2012CafeUtil.COFFEE_TYPE;
+   			caffeineContent = Rich2012CafeUtil.COFFEE_CAFFEINE_CONTENT;
+   			
+   		} else if(nameForComparison.contains("tea")){
+   			//Tea Product
+   			
+   			productType = Rich2012CafeUtil.TEA_TYPE;
+   			caffeineContent = Rich2012CafeUtil.TEA_CAFFEINE_CONTENT;
+   			
+   		} else if(nameForComparison.contains("relentless") || nameForComparison.contains("powerade") 
+   				|| nameForComparison.contains("lucozade") || nameForComparison.contains("red bull")){
+   			//Enery Drink Product
+   			
+   			productType = Rich2012CafeUtil.ENERGY_DRINK_TYPE;
+   			caffeineContent = Rich2012CafeUtil.ENERGY_DRINK_CAFFEINE_CONTENT;
+   			
+   		} else{
+   			//All others such as frappe, cappuchino, americano, latte, esspresso
+   			
+   			productType = Rich2012CafeUtil.COFFEE_TYPE;
+   			caffeineContent = Rich2012CafeUtil.COFFEE_CAFFEINE_CONTENT;
+   		}
+	   		
+   		return new CaffeineProduct(name, productType, caffeineContent);
+	}
+	
+	public ArrayList<CaffeineProduct> getCaffeineProducts(){
 		return products;
 	}
 }

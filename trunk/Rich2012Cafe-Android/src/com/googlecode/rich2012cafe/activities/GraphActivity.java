660c5b2f0bbf03788e971d4d3ff1792ddc5eded8
p300ss <p300ss@ef551103-5f9e-998d-499b-924edff78dac>
2012-04-30 13:57:15 +0000

    Graph Activity not libs now, src

--- trunk/Rich2012Cafe-Android/src/com/googlecode/rich2012cafe/activities/GraphActivity.java
+++ trunk/Rich2012Cafe-Android/src/com/googlecode/rich2012cafe/activities/GraphActivity.java
@@ -1,7 +1,15 @@
 package com.googlecode.rich2012cafe.activities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 import com.googlecode.rich2012cafe.R;
 import com.googlecode.rich2012cafe.Rich2012CafeActivity;
+import com.googlecode.rich2012cafe.model.CaffeineLevelReader;
+import com.googlecode.rich2012cafe.utils.Rich2012CafeUtil;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphView.GraphViewSeries;
@@ -12,6 +20,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -26,34 +35,53 @@ public class GraphActivity extends Activity{
         this.getActionBar().setHomeButtonEnabled(true);
 
 	    /* 
-	     * first init data 
-	     */  
-	    // sin curve  
-	    int num = 150;  
-	    GraphViewData[] data = new GraphViewData[num];  
-	    double v=0;  
-	    for (int i=0; i<num; i++) {  
-	       v += 0.2;  
-	       data[i] = new GraphViewData(i, Math.sin(v));  
-	    }  
-	    GraphViewSeries seriesSin = new GraphViewSeries("data",Color.rgb(255, 255, 216),data);
-	     	    
-	    /* 
-	     * create graph 
-	     */  
+//	     * first init data 
+//	     */  
+//	    // sin curve  
+//	    int num = 150;  
+//	    GraphViewData[] data = new GraphViewData[num];  
+//	    double v=0;  
+//	    for (int i=0; i<num; i++) {  
+//	       v += 0.2;  
+//	       data[i] = new GraphViewData(i, Math.sin(v));  
+//	    }  
+//	    GraphViewSeries seriesSin = new GraphViewSeries("data",Color.rgb(255, 255, 216),data);
+//	     	    
+//	    /* 
+//	     * create graph 
+//	     */  
+
+        fillGraph();
+	}
+	
+	private void fillGraph(){
+		CaffeineLevelReader clr = new CaffeineLevelReader(this);
+		TreeMap<Date, Integer> levels = clr.getCaffeineLevels(Rich2012CafeUtil.HISTORIC_VALUES_SETTING_NAME);
+		SimpleDateFormat mTime = new SimpleDateFormat("kkmm");
+		GraphViewData[] data = new GraphViewData[levels.size()];
+		int pos = 0;
+		for(Map.Entry<Date, Integer> entry : levels.entrySet()){
+			double time = Double.parseDouble(mTime.format(entry.getKey()));
+			//data[pos] = new GraphViewData(time, entry.getValue());
+			data[pos] = new GraphViewData(entry.getKey(), entry.getValue());
+			Log.i("Graph output", time+":  "+entry.getValue());
+			pos++;
+		}
+		GraphViewSeries seriesHistoric = new GraphViewSeries("Historic Levels",Color.rgb(255, 255, 216),data);
+		
 	    GraphView graphView = new LineGraphView(  
-	          this  
-	          , "EstimatedCaffeine Status Graph"  
-	    );  
-	    // add data 
-	    graphView.addSeries(seriesSin);  
-	    // optional - set view port, start=2, size=10  
-	    graphView.setViewPort(2, 10);  
-	    graphView.setScalable(true);  
-	    // optional - legend    
-	      
-	    LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);  
-	    layout.addView(graphView);  
+		          this  
+		          , "EstimatedCaffeine Status Graph"  
+		    );  
+		    // add data 
+		    graphView.addSeries(seriesHistoric);  
+		    // optional - set view port, start=2, size=10  
+		    graphView.setViewPort(2, 10);  
+		    graphView.setScalable(true);  
+		    // optional - legend    
+		      
+		    LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);  
+		    layout.addView(graphView);  
 	}
 	
     @Override

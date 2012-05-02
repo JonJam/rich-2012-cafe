666200919f8cc015e4549a6fbb64878536dd0f20
jonjam1990@googlemail.com <jonjam1990@googlemail.com@ef551103-5f9e-998d-499b-924edff78dac>
2012-05-02 16:40:25 +0000

    Altered getCaffeineSourcesGiven  in DataStore to only return sources that are currently open. Added isOpen method to DataStore to work out whether a source is currently open.

--- trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/Rich2012CafeService.java
+++ trunk/Rich2012Cafe-AppEngine/src/com/googlecode/rich2012cafe/server/Rich2012CafeService.java
@@ -20,7 +20,9 @@ public class Rich2012CafeService {
 	private static DataStore db = new DataStore();
 		
 	/**
-	 * Method get caffeine sources and their information given latitude and longitude position.
+	 * Method get caffeine sources and their information given:
+	 *  - Latitude and longitude position.
+	 *  - Whether sources are currently open.
 	 * 
 	 * @param latitude (double value)
 	 * @param longitude (double value)
@@ -30,29 +32,7 @@ public class Rich2012CafeService {
 	public List<CaffeineSourceWrapper> getCaffeineSourcesGiven(double latitude, double longitude){
 		return db.getCaffeineSourcesGiven(latitude, longitude);
 	}
-	
-	/**
-	 * Method to get CaffeineSourceProducts for a CaffeineSource.
-	 * 
-	 * @param id (String object)
-	 * @return List of CaffeineSourceProduct objects.
-	 */
-	@ServiceMethod
-	public List<CaffeineSourceProduct> getCaffeineSourceProductsForCaffeineSource(String id){
-		return db.getCaffeineSourceProductsForCaffeineSource(id);
-	}
-	
-	/**
-	 * Method to get OpeningTimes for a CaffeineSource.
-	 * 
-	 * @param id (String object)
-	 * @return List of OpeningTime objects.
-	 */
-	@ServiceMethod
-	public List<OpeningTime> getOpeningTimesForCaffeineSource(String id){
-		return db.getOpeningTimesForCaffeineSource(id);
-	}
-	
+		
 	/**
 	 * Method to get all CaffeineProducts.
 	 * 
